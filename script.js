b8cea37d819b2f94b07a5920b6a57123c145e118
Sephydev <nicolas.bussiere@outlook.fr>
2025-12-26 14:48:52 +0100

    feat(rock-paper-scissors): Implement scoring

--- script.js
+++ script.js
@@ -33,12 +33,19 @@ function playRound(humanChoice, computerChoice) {
   
   if (losingCondition) {
     console.log(`You lose! ${computerChoice} beats ${humanChoice}!`);
+    computerScore += 1;
   } else if (winningCondition) {
-    console.log(`You won! ${humanChoice} beats ${computerChoice}!`)
+    console.log(`You won! ${humanChoice} beats ${computerChoice}!`);
+    humanScore += 1;
   } else {
     console.log("it's a tie!");
   }
+
+  console.log(`Score: Human: ${humanScore} | Computer: ${computerScore}`)
 }
+
+let humanScore = 0;
+let computerScore = 0;
 
 const humanSelection = getHumanChoice();
 console.log("Human:", humanSelection);
