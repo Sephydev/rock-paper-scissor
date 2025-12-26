c3c1f434a4b9937e521d3dcbed669e63e657b7bd
Sephydev <nicolas.bussiere@outlook.fr>
2025-12-26 14:14:00 +0100

    feat(rock-paper-scissor): Get the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello World!")
+function getComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+
+  switch(randomNumber) {
+    case 1 :
+      return "Rock";
+      break;
+    case 2 :
+      return "Paper";
+      break;
+    case 3 :
+      return "Scissors";
+  }
+}
+
+console.log(getComputerChoice());
