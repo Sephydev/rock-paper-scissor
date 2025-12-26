759a2219515f80d7ea1c955b4399dbd7343f3578
Sephydev <nicolas.bussiere@outlook.fr>
2025-12-26 14:46:12 +0100

    feat(rock-paper-scissors): Add winner announcement

--- script.js
+++ script.js
@@ -16,8 +16,33 @@ function getComputerChoice() {
 function getHumanChoice() {
   const humanChoice = prompt("Please make a choice between 'Rock', 'Paper' or 'Scissors'.")
 
-  return humanChoice.toLowerCase();
+  return humanChoice;
 }
 
-console.log("Human:", getHumanChoice());
-console.log("Computer:", getComputerChoice());
+function playRound(humanChoice, computerChoice) {
+  const humanChoiceLower = humanChoice.toLowerCase();
+  const computerChoiceLower = computerChoice.toLowerCase();
+  
+  const losingCondition = humanChoiceLower === "rock" && computerChoiceLower === "paper" 
+  || humanChoiceLower === "paper" && computerChoiceLower === "scissors"
+  || humanChoiceLower === "scissors" && computerChoiceLower === "rock";
+  
+  const winningCondition = humanChoiceLower === "rock" && computerChoiceLower === "scissors"
+  || humanChoiceLower === "paper" && computerChoiceLower === "rock"
+  || humanChoiceLower === "scissors" && computerChoiceLower === "paper";
+  
+  if (losingCondition) {
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}!`);
+  } else if (winningCondition) {
+    console.log(`You won! ${humanChoice} beats ${computerChoice}!`)
+  } else {
+    console.log("it's a tie!");
+  }
+}
+
+const humanSelection = getHumanChoice();
+console.log("Human:", humanSelection);
+const computerSelection = getComputerChoice();
+console.log("Computer:", computerSelection)
+
+playRound(humanSelection, computerSelection);
